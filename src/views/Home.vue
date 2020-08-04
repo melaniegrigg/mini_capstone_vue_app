3b0214c7bde33ad14bff3a99609f61fe2578321c
melaniegrigg <melanie.grigger@gmail.com>
2020-08-03 21:17:29 -0400

    add index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="home">
-    <h1>How now Brown Cow. The arsonist has oddly shaped feet.</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-        <h1>The human torch was denied a bank loan.</h1>
-    <HelloWorld msg="Welcomes .js App"/>
+    <h1>{{ message }}</h1>
+    <h1>{{ products }}</h1>
+
+    <div v-for="product in products">
+      <p>Name: {{product.name}}</p>
+      <p>Description: {{product.description}}</p>
+      <p>Price: {{product.price}}</p>
+      <hr>
+    </div>
+      
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("printing important stuffs");
+      console.log("indexing......");
+
+      axios.get("/api/products").then((response) => {
+        console.log(response);
+        this.products = response.data;
+      });
+    },
   },
 };
 </script>

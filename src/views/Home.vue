8d382ed32421c6da4b234f09d940dd145b0bbf65
melaniegrigg <melanie.grigger@gmail.com>
2020-08-03 21:55:34 -0400

    add create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h1>{{ products }}</h1>
-
+     <p>Create a new product</p>
+          <button v-on:click="addProduct">Add a product</button>
+    
     <div v-for="product in products">
       <p>Name: {{product.name}}</p>
       <p>Description: {{product.description}}</p>
       <p>Price: {{product.price}}</p>
+      <img v-bind:src="product.image_url">
       <hr>
     </div>
-      
+  
   </div>
 </template>
 
@@ -22,13 +24,14 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Here Are Some Products!",
       products: [],
     };
   },
   created: function () {
     this.indexProducts();
   },
+
   methods: {
     indexProducts: function () {
       console.log("printing important stuffs");
@@ -37,6 +40,20 @@ export default {
       axios.get("/api/products").then((response) => {
         console.log(response);
         this.products = response.data;
+      });
+    },
+    addProduct: function () {
+      console.log("adding...");
+
+      var params = {
+        name: "NEW NEW",
+        description: "super new",
+        price: 90000,
+      };
+
+      axios.post("/api/products", params).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
       });
     },
   },

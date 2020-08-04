21c22ffd2a45dd510115fb6c76370ab6117f8953
melaniegrigg <melanie.grigger@gmail.com>
2020-08-03 20:16:29 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
+    <h1>How now Brown Cow. The arsonist has oddly shaped feet.</h1>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <h1>The human torch was denied a bank loan.</h1>
+    <HelloWorld msg="Welcomes .js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>

ad22f7a68afeb7f2f904775b7ebb0b4cc2897167
melaniegrigg <melanie.grigger@gmail.com>
2020-08-04 20:29:13 -0400

    add show more info button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,32 @@
   <div class="home">
     <h1>{{ message }}</h1>
      <p>Create a new product</p>
+     <p>Name:<input v-model="newProductName" type="text"></p>
+     <p>Description:<input v-model="newProductDescription" type="text"></p>
+     <p>Price:<input v-model="newProductPrice" type="text"></p>
+     <p>Image:<input v-model="newProductImage" type="text"></p>
           <button v-on:click="addProduct">Add a product</button>
     
     <div v-for="product in products">
       <p>Name: {{product.name}}</p>
-      <p>Description: {{product.description}}</p>
-      <p>Price: {{product.price}}</p>
+      <!-- <p>Description: {{product.description}}</p>
+      <p>Price: {{product.price}}</p> -->
+      <!-- <p>Image_url: {{product.image_url}}</p> -->
       <img v-bind:src="product.image_url">
+      <button v-on:click="showInfo(product)">Show more info</button>
       <hr>
     </div>
   
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product info</h1>
+        <p>Name: {{currentProduct.name}}</p>
+        <p>Description: {{currentProduct.description}}</p>
+        <p>Price: {{currentProduct.price}}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+
   </div>
 </template>
 
@@ -26,6 +42,11 @@ export default {
     return {
       message: "Here Are Some Products!",
       products: [],
+      newProductName: "",
+      newProductPrice: "",
+      newProductDescription: "",
+      newProductImage: "",
+      currentProduct: {},
     };
   },
   created: function () {
@@ -44,17 +65,24 @@ export default {
     },
     addProduct: function () {
       console.log("adding...");
+      console.log(this.newProductName);
 
       var params = {
-        name: "NEW NEW",
-        description: "super new",
-        price: 90000,
+        name: this.newProductName,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image: this.newProductImage,
       };
 
       axios.post("/api/products", params).then((response) => {
         console.log(response.data);
         this.products.push(response.data);
       });
+    },
+    showInfo: function (product) {
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector("#product-details").showModal();
     },
   },
 };
